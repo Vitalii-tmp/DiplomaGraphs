e7320d843a65efae0568a55623385ecef193dbb6
Vitalii-tmp <vitaliilchuk6@gmail.com>
2024-05-18 23:22:30 +0200

    init

---
+++ diploma/diploma/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace diploma.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
